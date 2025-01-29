87a83873a010cf3d2bbd59d4084a066e983ca9cf
sarf01k <isaacksarfo1@gmail.com>
2025-01-29 13:32:25 +0000

    update

---
+++ README.md
@@ -0,0 +1,46 @@
+# HNG 12 Stage 0 Task
+
+HNG 12 Stage 0 backend task.
+
+## Description
+
+An API that returns a HNG 12 intern's info.
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en)
+- **npm**
+
+## Installation
+
+1. Clone the repository:
+   ```
+   git clone https://github.com/sarf01k/HNG_stage_0
+   cd HNG_stage_0
+   ```
+2. Install dependencies:
+   ```
+   npm install
+   ```
+
+## Usage
+
+    `npm` run start
+
+## API Endpoints
+
+**GET** '`/`'
+
+#### Sample Response:
+
+```json
+{
+  "email": "user@email.com",
+  "current_datetime": "2025-01-29T12:32:09.374Z",
+  "github_url": "https://github.com/user/repo"
+}
+```
+
+### Backlink
+
+- https://hng.tech/hire/nodejs-developers
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-const port = 3000;
+const corsOptions = {
+    origin: '*',
+    methods: ['GET'],
+    allowedHeaders: ['*']
+};
+
+const port = 3000 || process.env.PORT;
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
     res.status(200).json({
@@ -10,6 +20,11 @@ app.get('/', async (req, res) => {
         current_datetime: new Date().toISOString(),
         github_url: 'https://github.com/sarf01k/HNG_stage_0'
     })
+});
+
+app.use((err, req, res) => {
+    console.error(err);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
 app.listen(port, () => {
